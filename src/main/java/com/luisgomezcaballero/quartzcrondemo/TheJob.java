acf3140aaac922171c88aa388225d992e5a8d065
Luis Gómez Caballero <luis.gomez.caballero@gmail.com>
2019-09-08 18:16:39 +0200

    Project upload

---
+++ src/main/java/com/luisgomezcaballero/quartzcrondemo/TheJob.java
@@ -0,0 +1,13 @@
+package com.luisgomezcaballero.quartzcrondemo;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class TheJob implements Job {
+	
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		System.out.println("Hello world.");
+	}
+	
+}
